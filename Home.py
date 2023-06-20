8cbe4533df56a4460d65c5387eec852d1e065037
Luisa Fernanda Gómez Ossa <lfgomezos@unal.edu.co>
2023-06-19 22:00:49 -0500

    map

--- Home.py
+++ Home.py
@@ -2,10 +2,13 @@ import streamlit as st
 import leafmap.foliumap as leafmap
 
 st.set_page_config(layout='wide')
+dropdown = st.selectbox("Basemap", ["HYBRID", "ROADMAP", "TERRAIN", "SATELLITE"])
 
 m = leafmap.Map()
-m.add_basemap('HYBRID')
+m.add_basemap(dropdown)
+st.text('this is the main map to development')
+
 m.to_streamlit()
 
-st.text('this is the main map to development')
+
 
